e8da143e681af2d81ca4b500b2635693fccf38c7
dismond5 <omarkhaled201148@gmail.com>
2023-05-13 08:29:26 -0700

    A

---
+++ models/engine/file_storage.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""class FileStorage serializess and deserialize JSONfiles"""
+import uuid
+import os
+import json
+from datetime import datetime
+from models.base_model import BaseModel
+from models.state import State
+from models.user import User
+from models.review import Review
+from models.place import Place
+from models.amenity import Amenity
+from models.city import City
+
+
+class FileStorage:
+    """ construct """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ return dictionary objects """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """ sets in dictionary the obj with key <obj class name>.id """
+        FileStorage.__objects[obj.__class__.__name__ + "." + str(obj.id)] = obj
+
+    def save(self):
+        """ serializes objectss to the JSON file (path: __file_path) """
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as fname:
+            new_dict = {key: obj.to_dict() for key, obj in
+                        FileStorage.__objects.items()}
+            json.dump(new_dict, fname)
+
+    def reload(self):
+        """ Reload the file """
+        if (os.path.isfile(FileStorage.__file_path)):
+            with open(FileStorage.__file_path, 'r', encoding="utf-8") as fname:
+                l_json = json.load(fname)
+                for key, val in l_json.items():
+                    FileStorage.__objects[key] = eval(
+                        val['__class__'])(**val)
